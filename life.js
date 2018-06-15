aed7a55e88e58bf00c826a62e0c755227d6a890d
Ivan Mikryakov <imikryakov@it.ru>
2018-06-15 12:37:39 +0300

    code organization

--- life.js
+++ life.js
@@ -21,61 +21,26 @@ var modeAutomatic = document.getElementById('modeAutomatic');
 var field = document.getElementById('field');
 var generationLabel = document.getElementById('generationLabel');
 
-function modeManualClick() {
-    stop();
-    startButton.disabled = true;
-    stopButton.disabled = true;
-    intervalInput.disabled = true;
-    nextStepButton.disabled = false;
+/* random numbers generation helpers */
+
+function random0or1() {
+    return Math.round(Math.random());
 }
 
-function modeAutomaticClick() {
-    startButton.disabled = false;
-    stopButton.disabled = true;
-    intervalInput.disabled = false;
-    nextStepButton.disabled = true;
+function randomMinus1or1() {
+    var r = Math.round(Math.random()) ? 1 : -1; 
+    console.log(r);
+    return r;
 }
 
-function start() {
-    stop();
-    startButton.disabled = true;
-    stopButton.disabled = false;
-    interval = Number(intervalInput.value);
-    timerId = setInterval(function() { nextGeneration() }, interval * 1000);
+function randomUpToN(n) {
+    return Math.floor(Math.random() * n);
 }
 
-function stop() {
-    startButton.disabled = false;
-    stopButton.disabled = true;
-    if (timerId != undefined) {
-        clearInterval(timerId);
-    }
-}
+/* cell manipulation helpers */
 
-function nextStep() {
-    nextGeneration();
-}
-
-function clear() {
-    for (var r = 0; r < size; r++) 
-        for (var c = 0; c < size; c++) 
-            makeDead(cells[r][c]);
-}
-
-function reconstruct() {
-    if (modeAutomatic.checked)
-        stop();
-    generation = 0;
-    updateGenerationLabel();
-    var l = field.children.length;
-    for (var i = 0; i < l; i++) {
-        field.removeChild(field.children[0]);
-    }
-    constructField();
-}
-
-function cellClick() {
-    this.classList.toggle("alive");
+function toggleAlive(cell) {
+    cell.classList.toggle("alive");
 }
 
 function isAlive(cell) {
@@ -96,59 +61,10 @@ function makeDead(cell) {
         cell.click();
 }
 
+/* other helpers */
+
 function updateGenerationLabel() {
     generationLabel.innerHTML = "generation: " + generation;
-}
-
-function constructField() {
-    size = Number(sizeInput.value);
-    cells = new Array(size);
-    for (var r = 0; r < size; r++) {
-        cells[r] = new Array(size);
-        var cellRow = document.createElement("div");
-        cellRow.className ="cellRow";
-        field.appendChild(cellRow);
-        for (var c = 0; c < size; c++) {
-            var cell = document.createElement("div");
-            cell.className = "cell";
-            cell.row = r;
-            cell.col = c;
-            cell.onclick = cellClick;
-            cellRow.appendChild(cell);
-            cells[r][c] = cell;  
-        }   
-    }
-}
-
-function nextGeneration() {
-    console.log("nextGeneration");
-    var newGen = Array(size);
-    for (var r = 0; r < size; r++) {
-        newGen[r] = new Array(size);
-        for (var c = 0; c < size; c++) {
-            var cell = cells[r][c];
-            var n = getAliveNeighborsCount(cell)
-            if (isDead(cell) && n == 3) {
-                newGen[r][c] = true;
-            } else 
-            if (isAlive(cell) && n != 2 && n != 3) {
-                newGen[r][c] = false;
-            } else {
-                newGen[r][c] = isAlive(cell)
-            }
-        }
-    }
-    for (var r = 0; r < size; r++) {
-        for (var c = 0; c < size; c++) {
-            if (newGen[r][c])
-                makeAlive(cells[r][c])
-            else
-                makeDead(cells[r][c]);
-        }
-    }
-    generation++;
-    updateGenerationLabel();
-
 }
 
 function getAliveNeighborsCount(cell) {
@@ -180,32 +96,6 @@ function normalizeCoord(coord) {
     return coord;
 }
 
-function random0or1() {
-    return Math.round(Math.random());
-}
-
-function randomMinus1or1() {
-    var r = Math.round(Math.random()) ? 1 : -1; 
-    console.log(r);
-    return r;
-}
-
-function randomUpToN(n) {
-    return Math.floor(Math.random() * n);
-}
-
-function random() {
-    clear();
-    for (var r = 0; r < size; r++) 
-        for (var c = 0; c < size; c++) {
-            if (random0or1()) {
-                makeAlive(cells[r][c]);
-            } else {
-                makeDead(cells[r][c]);
-            }
-        }
-}
-
 function drawMap(map) {
     var col = randomUpToN(size);
     var row = randomUpToN(size);
@@ -226,12 +116,133 @@ function drawMap(map) {
         }
 }
 
+function constructField() {
+    size = Number(sizeInput.value);
+    cells = new Array(size);
+    for (var r = 0; r < size; r++) {
+        cells[r] = new Array(size);
+        var cellRow = document.createElement("div");
+        cellRow.className ="cellRow";
+        field.appendChild(cellRow);
+        for (var c = 0; c < size; c++) {
+            var cell = document.createElement("div");
+            cell.className = "cell";
+            cell.row = r;
+            cell.col = c;
+            cell.onclick = cellClick;
+            cellRow.appendChild(cell);
+            cells[r][c] = cell;  
+        }   
+    }
+}
+
+/* basic logic */
+
+function nextGeneration() {
+    console.log("nextGeneration");
+    var newGen = Array(size);
+    for (var r = 0; r < size; r++) {
+        newGen[r] = new Array(size);
+        for (var c = 0; c < size; c++) {
+            var cell = cells[r][c];
+            var n = getAliveNeighborsCount(cell)
+            if (isDead(cell) && n == 3) {
+                newGen[r][c] = true;
+            } else 
+            if (isAlive(cell) && n != 2 && n != 3) {
+                newGen[r][c] = false;
+            } else {
+                newGen[r][c] = isAlive(cell)
+            }
+        }
+    }
+    for (var r = 0; r < size; r++) {
+        for (var c = 0; c < size; c++) {
+            if (newGen[r][c])
+                makeAlive(cells[r][c])
+            else
+                makeDead(cells[r][c]);
+        }
+    }
+    generation++;
+    updateGenerationLabel();
+}
+
+/* click handlers and actions */
+
+function cellClick() {
+    toggleAlive(this);
+}
+
+function modeManualClick() {
+    stop();
+    startButton.disabled = true;
+    stopButton.disabled = true;
+    intervalInput.disabled = true;
+    nextStepButton.disabled = false;
+}
+
+function modeAutomaticClick() {
+    startButton.disabled = false;
+    stopButton.disabled = true;
+    intervalInput.disabled = false;
+    nextStepButton.disabled = true;
+}
+
+function start() {
+    stop();
+    startButton.disabled = true;
+    stopButton.disabled = false;
+    interval = Number(intervalInput.value);
+    timerId = setInterval(function() { nextGeneration() }, interval * 1000);
+}
+
+function stop() {
+    startButton.disabled = false;
+    stopButton.disabled = true;
+    if (timerId != undefined) {
+        clearInterval(timerId);
+    }
+}
+
+function clear() {
+    for (var r = 0; r < size; r++) 
+        for (var c = 0; c < size; c++) 
+            makeDead(cells[r][c]);
+}
+
+function reconstruct() {
+    if (modeAutomatic.checked)
+        stop();
+    generation = 0;
+    updateGenerationLabel();
+    var l = field.children.length;
+    for (var i = 0; i < l; i++) {
+        field.removeChild(field.children[0]);
+    }
+    constructField();
+}
+
+function random() {
+    clear();
+    for (var r = 0; r < size; r++) 
+        for (var c = 0; c < size; c++) {
+            if (random0or1()) {
+                makeAlive(cells[r][c]);
+            } else {
+                makeDead(cells[r][c]);
+            }
+        }
+}
+
 function glider() {
     var map = ["x  ",
                "x x",
                "xx "];
     drawMap(map);               
 }
+
+/* define and run init function */
 
 function initialize() {
     sizeInput.value = DEFAULT_SIZE;
@@ -242,7 +253,7 @@ function initialize() {
     modeAutomatic.onclick = modeAutomaticClick;
     startButton.onclick = start;
     stopButton.onclick = stop;
-    nextStepButton.onclick = nextStep;
+    nextStepButton.onclick = nextGeneration;
     clearButton.onclick = clear;
     reconstructButton.onclick = reconstruct;
     randomButton.onclick = random;
